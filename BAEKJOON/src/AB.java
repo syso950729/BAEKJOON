5224298b0eea7f38b3add2487fc3bfe8071b0e36
syso950729 <syso950729@gmail.com>
2022-02-13 21:21:51 +0900

    백준 A+B
    -https://www.acmicpc.net/problem/1000

---
+++ BAEKJOON/src/AB.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class AB {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int A,B;
+		//System.out.println("A는?");
+		A = sc.nextInt();
+		//System.out.println("B는?");
+		B = sc.nextInt();
+		System.out.println(A+B);
+
+	}
+
+}
