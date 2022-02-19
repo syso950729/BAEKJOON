6859a99899431155e855bafd12b4de03c99bae39
syso950729 <syso950729@gmail.com>
2022-02-19 22:38:41 +0900

    220219 백준 if문

---
+++ BAEKJOON/src/IF/if2.java
@@ -0,0 +1,35 @@
+package IF;
+
+import java.util.Scanner;
+
+public class if2 {
+
+	public static void main(String[] args) {
+		/*문제
+		시험 점수를 입력받아 90 ~ 100점은 A, 80 ~ 89점은 B, 70 ~ 79점은 C, 60 ~ 69점은 D, 나머지 점수는 F를 출력하는 프로그램을 작성하시오.
+
+		입력
+		첫째 줄에 시험 점수가 주어진다. 시험 점수는 0보다 크거나 같고, 100보다 작거나 같은 정수이다.
+
+		출력
+		시험 성적을 출력한다.
+		 */
+		
+		Scanner sc = new Scanner(System.in);
+		int total = sc.nextInt();
+		
+		if(total>=90) {
+			System.out.println("A");
+		}else if(total>=80)
+			System.out.println("B");
+		else if(total>=70) {
+			System.out.println("C");
+		}else if(total>=60) {
+			System.out.println("D");
+		}else {
+			System.out.println("F");
+		}
+
+	}
+
+}
